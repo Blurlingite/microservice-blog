54067fce044ac93e7373279c18a9ea1f78d24dfd
Vishnu Dawah <vkdawah@gmail.com>
2020-04-28 17:10:26 -0400

    Add query service

---
+++ query/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors);
+
+app.get("/posts", (req, res) => {});
+
+// receive events from event bus
+app.post("/events", (req, res) => {});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
